70120f77247a5eaa869600d986d73d5ccccaed6a
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-10-22 15:59:24 -0300

    Split document in sale/purchase subdocuments

--- nobix/models/document.py
+++ nobix/models/document.py
@@ -8,15 +8,22 @@ from sqlalchemy import UniqueConstraint
 
 from nobix.models import db
 
+DOC_SALE = 'sale'
+DOC_PURCHASE = 'purchase'
+doc_subtypes = [
+    DOC_SALE,
+    DOC_PURCHASE
+]
+
 class DocumentType(db.Model):
     __tablename__ = 'document_type'
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.Unicode, unique=True)
     name = db.Column(db.Unicode)
+    subtype = db.Column(db.Enum(*doc_subtypes, name='document_subtype_enum'))
 
 class Document(db.Model):
     __tablename__ = 'document'
-    __table_args__ = (UniqueConstraint('doc_type', 'issue_date', 'number'),)
 
     #: the document is in draft status
     STATUS_DRAFT = 'STATUS_DRAFT'
@@ -36,10 +43,11 @@ class Document(db.Model):
     }
 
     id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String)
     issue_date = db.Column(db.DateTime, nullable=False)
     expiration_date = db.Column(db.DateTime)
     number = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.Enum(*_statuses.keys(), name='document_status_enum',
+    status = db.Column(db.Enum(*_statuses.keys(), name='document_status_enum'),
                        default=STATUS_DRAFT, nullable=False)
 
     discount = db.Column(db.Numeric(10, 2), default=Decimal(0))
@@ -49,28 +57,15 @@ class Document(db.Model):
                             nullable=False)
     doc_type = db.relationship(DocumentType)
 
-    issue_place_id = db.Column(db.Integer, db.ForeignKey('place.id'),
-                               nullable=False)
-    issue_place = db.relationship('Place', backref=db.backref("documents",
-                                                              lazy="dynamic"))
-
-    salesman_id = db.Column(db.Integer, db.ForeignKey('user.id'),
-                            nullable=False)
-    salesman = db.relationship('User')
-
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'),
-                            nullable=False)
-    customer = db.relationship('Customer')
-
-    # Info extra documento
-    customer_name = db.Column(db.UnicodeText(35))
-    customer_address = db.Column(db.UnicodeText(60))
-    customer_cuit = db.Column(db.UnicodeText(13), nullable=True)
-
     #: items field added by DocumentItem model
     #: taxes field added by TaxItem model
     #: payments field added by Pyment model
     #: book_entry field added by BookEntry model
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'base',
+        'polymorphic_on': type,
+    }
 
     @property
     def doc_type(self):
@@ -88,13 +83,50 @@ class Document(db.Model):
         return Decimal(self.net if self.net is not None else 0) +
                Decimal(sum(t.amount) for t in self.taxes)
 
+
+class SaleDocument(Document):
+    __tablename__ = 'sale_document'
+    __mapper_args__ = {'polymorphic_identity': DOC_SALE}
+
+    id = db.Column(db.Integer, db.ForeignKey('document.id'), primary_key=True)
+
+    #issue_place_id = db.Column(db.Integer, db.ForeignKey('place.id'),
+    #                           nullable=False)
+    #issue_place = db.relationship('Place', backref=db.backref("documents",
+    #                                                          lazy="dynamic"))
+
+    #salesman_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+    #                        nullable=False)
+    #salesman = db.relationship('User')
+
+    #customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'),
+    #                        nullable=False)
+    #customer = db.relationship('Customer')
+
+    # Info extra documento
+    customer_name = db.Column(db.UnicodeText(35))
+    customer_address = db.Column(db.UnicodeText(60))
+    customer_cuit = db.Column(db.UnicodeText(13), nullable=True)
+
     def __str__(self):
         return "{} {}-{}".format(self.doc_type.name,
                                  self.issue_place.pos_number,
                                  self.number)
 
     def __repr__(self):
-        return "<Document {} '{}'>".format(self.status, str(self))
+        return "<SaleDocument {} '{}'>".format(self.status, str(self))
+
+
+class PurchaseDocument(Document):
+    __tablename__ = 'purchase_document'
+    __mapper_args__ = {'polymorphic_identity': DOC_PURCHASE}
+
+    id = db.Column(db.Integer, db.ForeignKey('document.id'), primary_key=True)
+
+    #supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'),
+    #                        nullable=False)
+    #supplier = db.relationship('Supplier')
+    pos_number = db.Column(db.Integer)
 
 
 class DocumentItem(db.Model):
@@ -109,9 +141,9 @@ class DocumentItem(db.Model):
     sku = db.Column(db.Unicode)
     price = db.Column(db.Numeric(10, 2))
 
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
-    product = db.relationship('Product', backref=db.backref('items',
-                                                            lazy='dynamic'))
+    #product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    #product = db.relationship('Product', backref=db.backref('items',
+    #                                                        lazy='dynamic'))
 
     def __repr__(self):
         return "<Item '{}' x '{}' in '{}'>".format(self.product, self.quantity,
